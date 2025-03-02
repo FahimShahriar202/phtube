8fc4de199ad87972c7af0195cc5d631165f187e4
FahimShahriar202 <fahimshahriar202@gmail.com>
2025-03-02 20:49:09 +0600

    no content found added

--- script/video.js
+++ script/video.js
@@ -106,6 +106,21 @@ const displayVideos = (videos) => {
 
   videoContainer.innerHTML='';
 
+  if (videos.length ==0){
+    videoContainer.classList.remove("grid");
+    videoContainer.innerHTML = `
+     <div class = "min-h-[300px] w-full flex flex-col gap-5 justify-center items-center">
+      <img src="assets/Icon.png"/>
+
+      <h2 class= "text-center text-xl font-bold">No Content Found</h2>
+     </div>`;
+     return;
+  }
+
+  else{
+    videoContainer.classList.add("grid");
+  }
+
   videos.forEach(video => {
     console.log(video);
 
